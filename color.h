71278ddadddac8e5887dec04ad24ea8197e19ea7
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2025-05-30 15:50:00 +0200

    added sphere

--- color.h
+++ color.h
@@ -8,14 +8,14 @@
 using color = vec3;
 
 void write_color(std::ostream& out, const color& pixel_color) {
-    auto r = pixel_color.x();
-    auto g = pixel_color.y();
-    auto b = pixel_color.z();
+    const auto r = pixel_color.x();
+    const auto g = pixel_color.y();
+    const auto b = pixel_color.z();
 
     // Translate the [0,1] component values to the byte range [0,255].
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+    const int rbyte = static_cast<int>(255.999 * r);
+    const int gbyte = static_cast<int>(255.999 * g);
+    const int bbyte = static_cast<int>(255.999 * b);
 
     // Write out the pixel color components.
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
--- vec3.h
+++ vec3.h
@@ -8,7 +8,7 @@ class vec3 {
   public:
     double e[3];
 
-    vec3() : e{0,0,0} {}
+    vec3() : e{0, 0, 0} {}
     vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
     double x() const { return e[0]; }
@@ -16,6 +16,7 @@ class vec3 {
     double z() const { return e[2]; }
 
     vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+
     double operator[](int i) const { return e[i]; }
     double& operator[](int i) { return e[i]; }
 
@@ -34,7 +35,7 @@ class vec3 {
     }
 
     vec3& operator/=(double t) {
-        return *this *= 1/t;
+        return *this *= (1 / t);
     }
 
     double length() const {
@@ -42,13 +43,12 @@ class vec3 {
     }
 
     double length_squared() const {
-        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
     }
 };
 
 // point3 is just an alias for vec3, but useful for geometric clarity in the code.
 using point3 = vec3;
-
 
 // Vector Utility Functions
 
@@ -69,7 +69,7 @@ inline vec3 operator*(const vec3& u, const vec3& v) {
 }
 
 inline vec3 operator*(double t, const vec3& v) {
-    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
 inline vec3 operator*(const vec3& v, double t) {
@@ -77,7 +77,7 @@ inline vec3 operator*(const vec3& v, double t) {
 }
 
 inline vec3 operator/(const vec3& v, double t) {
-    return (1/t) * v;
+    return (1 / t) * v;
 }
 
 inline double dot(const vec3& u, const vec3& v) {
@@ -87,9 +87,11 @@ inline double dot(const vec3& u, const vec3& v) {
 }
 
 inline vec3 cross(const vec3& u, const vec3& v) {
-    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
-                u.e[2] * v.e[0] - u.e[0] * v.e[2],
-                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+    return vec3(
+        u.e[1] * v.e[2] - u.e[2] * v.e[1],
+        u.e[2] * v.e[0] - u.e[0] * v.e[2],
+        u.e[0] * v.e[1] - u.e[1] * v.e[0]
+    );
 }
 
 inline vec3 unit_vector(const vec3& v) {
